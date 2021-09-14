42dd0b5b03d416aa8435d61ea5babf425cfa35d4
FabricioCespedes <fabriciocespedes16@gmail.com>
2021-09-14 11:51:17 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatInputModule} from '@angular/material/input';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ModuloComponent } from './components/modulo/modulo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ModuloComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
